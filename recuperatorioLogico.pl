cf06050ae163827ed30a9657baf74de6429b8690
Alvaro Signorio <signorioalvaro@gmail.com>
2023-12-20 20:10:24 -0300

    cuarto commit

--- recuperatorioLogico.pl
+++ recuperatorioLogico.pl
@@ -12,10 +12,13 @@ pieza(torre, negra, 3,4).
 pieza(reina, negra, 5,5).
 
 pieza(alfil, blanca, 5, 5).
-pieza(torre, blanca, 2, 9).
+
 
 casilleroLibre(Fil, Col):- not(pieza(_, _, Fil, Col)), not(pieza(_, _, Fil, Col)).
 
 dosPiezasEnMismoCasillero(Fil, Col):-pieza(Pieza1, _, Fil, Col), pieza(Pieza2, _, Fil, Col), Pieza1 \=Pieza2. 
 
 piezaEnCasilleroInvalido(Pieza, _, Fil, Col):- not(nro(Fil)) | not(nro(Col)). 
+
+elementoExtranio(Pieza, Color, _, _):- not(pieza(Pieza, Color, _, _)).
+
