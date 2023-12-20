0c491048ca76767d18fcb2dfb945a17547659231
Alvaro Signorio <signorioalvaro@gmail.com>
2023-12-20 20:44:58 -0300

    terminado los tres puntos

--- recuperatorioLogico.pl
+++ recuperatorioLogico.pl
@@ -12,13 +12,40 @@ pieza(torre, negra, 3,4).
 pieza(reina, negra, 5,5).
 
 pieza(alfil, blanca, 5, 5).
+pieza(peon, negra, 2, 5).
 
 
+% devuelve verdadero si hay dos piezas en el mismo casillero
 casilleroLibre(Fil, Col):- not(pieza(_, _, Fil, Col)), not(pieza(_, _, Fil, Col)).
 
-dosPiezasEnMismoCasillero(Fil, Col):-pieza(Pieza1, _, Fil, Col), pieza(Pieza2, _, Fil, Col), Pieza1 \=Pieza2. 
 
-piezaEnCasilleroInvalido(Pieza, _, Fil, Col):- not(nro(Fil)) | not(nro(Col)). 
+% devuelve verdadero si hay dos piezas en el mismo casillero
+dosPiezasEnMismoCasillero(Pieza, Color, Fil, Col):-pieza(Pieza1, Color, Fil, Col), pieza(Pieza2, Color, Fil, Col), Pieza1 \=Pieza2. 
 
+
+% devuelve verdadero si hay una pieza fuera del tablero
+piezaEnCasilleroInvalido(Pieza, Color, Fil, Col):- not(nro(Fil)) | not(nro(Col)). 
+
+
+% devuelve verdadero si hay una pieza extraña
 elementoExtranio(Pieza, Color, _, _):- not(pieza(Pieza, Color, _, _)).
+
+dosPiezasDelMismoColor(Pieza1, Pieza2, Color1, Color2):- pieza(Pieza1, Color1, _, _), 
+    pieza(Pieza2, Color2, _,_), 
+    Color1==Color2.
+
+
+
+% 3) 
+puedeMoverseTorre(torre, Color, Fil, Col, FilLlegada, Col):- nro(FilLlegada),
+    FilLlegada \= Fil,
+    not(pieza(Pieza, Color, FilLlegada, Col)).
+
+puedeMoverseTorre(torre, Color, Fil, Col, Fil, ColLegada):- nro(ColLegada),
+    ColLegada \= Col,
+    not(pieza(Pieza, Color, Fil, ColLegada)).
+
+    
+
+
 
