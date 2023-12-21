e934cac3aa746bf164e79494ff3771b323cd0b8d
Alvaro Signorio <signorioalvaro@gmail.com>
2023-12-20 22:32:01 -0300

    terminado

--- recuperatorioLogico.pl
+++ recuperatorioLogico.pl
@@ -16,6 +16,7 @@ pieza(reina, negra, 5,5).
 
 pieza(alfil, blanca, 5, 5).
 pieza(peon, negra, 2, 5).
+pieza(peon,blanca, 2, 4).
 
 % 1)
 % devuelve verdadero si hay dos piezas en el mismo casillero
@@ -37,17 +38,38 @@ informacionErronea(Pieza, Color, Fil, Col):- not(pieza(Pieza, Color, Fil, Col));
     not(color(Color)).
 
 % devuelve verdadero si hay una pieza extraña
-elementoExtranio(Pieza, Color, Fil, Col):- not(informacionErronea(Pieza, Color, Fil, Col)).
+elementoExtranio(Pieza, Color, Fil, Col):- informacionErronea(Pieza, Color, Fil, Col).
+
+
 
 % 3) 
-puedeMoverseTorre(torre, Color, Fil, Col, FilLlegada, Col):- nro(FilLlegada),
+puedeMoverseTorre(torre, Color, Fil, Col, FilLlegada, Col):- not(elementoExtranio(torre, Color, Fil, Col)), 
+    not(elementoExtranio(torre, Color, FilLlegada, Col)),
     FilLlegada \= Fil,
-    not(pieza(Pieza, Color, FilLlegada, Col)).
+    not(pieza(Pieza, Color, FilLlegada, Col));
+    pieza(Pieza, OtroColor, FilLlegada, Col), piezasDeDistintoColor(torre, Color, Pieza, OtroColor).
 
-puedeMoverseTorre(torre, Color, Fil, Col, Fil, ColLegada):- nro(ColLegada),
-    ColLegada \= Col,
+puedeMoverseTorre(torre, Color, Fil, Col, Fil, ColLlegada):- not(elementoExtranio(torre, Color, Fil, Col)), 
+    not(elementoExtranio(torre, Color, Fil, ColLlegada)),
+    ColLlegada \= Col,
+    not(pieza(Pieza, Color, Fil, ColLlegada));
+    pieza(Pieza, OtroColor, Fil, ColLlegada), piezasDeDistintoColor(torre, Color, Pieza, OtroColor).
+
+
+
+piezasDeDistintoColor(Pieza1, Color1, Pieza2, Color2):-pieza(Pieza1, Color1, _, _),
+    pieza(Pieza2, Color2, _, _),
+    Color1 \= Color2.
+
+
+% devuelve verdadero si se puede mover sin comer
+puedeMoverseSinComer(torre, Color, Fil, Col, FilLlegada, Col) :- not(elementoExtranio(torre, Color, Fil, Col)), 
+    not(elementoExtranio(torre, Color, FilLlegada, Col)),
+    FilLlegada \= Col, 
+    not(pieza(Pieza, Color, Fil, FilLlegada)).
+
+puedeMoverseSinComer(torre, Color, Fil, Col, Fil, ColLegada) :- not(elementoExtranio(torre, Color, Fil, Col)), 
+    not(elementoExtranio(torre, Color, ColLlegada, Col)),
+    ColLegada \= Col, 
     not(pieza(Pieza, Color, Fil, ColLegada)).
-
-
-
 
