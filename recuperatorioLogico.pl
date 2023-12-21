82ac194368d434f448a666b5855e8ab8fe49d5cf
Alvaro Signorio <signorioalvaro@gmail.com>
2023-12-20 21:19:17 -0300

    correcciones

--- recuperatorioLogico.pl
+++ recuperatorioLogico.pl
@@ -7,6 +7,9 @@ nro(6).
 nro(7).
 nro(8).
 
+color(negra).
+color(blanca).
+
 pieza(torre, negra, 2,1).
 pieza(torre, negra, 3,4).
 pieza(reina, negra, 5,5).
@@ -14,27 +17,27 @@ pieza(reina, negra, 5,5).
 pieza(alfil, blanca, 5, 5).
 pieza(peon, negra, 2, 5).
 
-
+% 1)
 % devuelve verdadero si hay dos piezas en el mismo casillero
 casilleroLibre(Fil, Col):- not(pieza(_, _, Fil, Col)), not(pieza(_, _, Fil, Col)).
 
-
+% 2)
 % devuelve verdadero si hay dos piezas en el mismo casillero
 dosPiezasEnMismoCasillero(Pieza, Color, Fil, Col):-pieza(Pieza1, Color, Fil, Col), pieza(Pieza2, Color, Fil, Col), Pieza1 \=Pieza2. 
 
 
 % devuelve verdadero si hay una pieza fuera del tablero
-piezaEnCasilleroInvalido(Pieza, Color, Fil, Col):- not(nro(Fil)) | not(nro(Col)). 
+piezaEnCasilleroInvalido(Pieza, Color, Fil, Col):- not(nro(Fil));
+    not(nro(Col)). 
 
+
+% devuelve verdadero si hay informacion ajena al ajedrez
+informacionErronea(Pieza, Color, Fil, Col):- not(pieza(Pieza, Color, Fil, Col));
+    piezaEnCasilleroInvalido(Pieza, Color, Fil, Col);
+    not(color(Color)).
 
 % devuelve verdadero si hay una pieza extraña
-elementoExtranio(Pieza, Color, _, _):- not(pieza(Pieza, Color, _, _)).
-
-dosPiezasDelMismoColor(Pieza1, Pieza2, Color1, Color2):- pieza(Pieza1, Color1, _, _), 
-    pieza(Pieza2, Color2, _,_), 
-    Color1==Color2.
-
-
+elementoExtranio(Pieza, Color, Fil, Col):- not(informacionErronea(Pieza, Color, Fil, Col)).
 
 % 3) 
 puedeMoverseTorre(torre, Color, Fil, Col, FilLlegada, Col):- nro(FilLlegada),
@@ -45,7 +48,6 @@ puedeMoverseTorre(torre, Color, Fil, Col, Fil, ColLegada):- nro(ColLegada),
     ColLegada \= Col,
     not(pieza(Pieza, Color, Fil, ColLegada)).
 
-    
 
 
 
